93d0ce6922abe987493e20c4e308b39dfd7a883c
Karl Matthes <karlmatthes@gmail.com>
2019-11-06 22:41:31 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -40,8 +40,7 @@ def apply_coupons(cart, coupons)
   coupons.length.times do |index|
     if coupons[index][:item] == find_item_by_name_in_collection(coupons[index][:item], cart)[:item]
       cart.length.times do |inner_index|
-        if (cart[inner_index][:item] == coupons[index][:item]) 
-          && (cart[inner_index][:count] - coupons[index][:num] >= 0)
+        if cart[inner_index][:item] == coupons[index][:item] && cart[inner_index][:count] - coupons[index][:num] >= 0
           coupon_item = coupons[index][:item] + " W/COUPON"
           temp_hash = {
             :item => coupon_item,
