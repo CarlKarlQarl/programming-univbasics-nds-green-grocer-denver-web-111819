c6cfa7ea267b26da5c2faa16a74af5cc9db895c3
Karl Matthes <karlmatthes@gmail.com>
2019-11-06 21:26:41 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -4,7 +4,7 @@ def find_item_by_name_in_collection(name, collection)
   
   collection.length.times do |index|
     if collection[index][:item] == name
-      return 
+      return collection[index]
     end
   end
   
