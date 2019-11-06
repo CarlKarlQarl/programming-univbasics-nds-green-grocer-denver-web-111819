00a70ac479310081b454d12f0ca2f41c419cfc9c
Karl Matthes <karlmatthes@gmail.com>
2019-11-06 22:10:20 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -37,8 +37,21 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   # REMEMBER: This method **should** update cart
   
-  
-  
+  coupons.length.times do |index|
+    if coupons[index][:item] == find_item_by_name_in_collection(coupons[index][:item], cart)[:item]
+      cart.length.times do |inner_index|
+        if cart[inner_index][:item] == coupons[index][:item] 
+          && cart[inner_index][:count] - coupons[index][:num] >= 0
+          coupon_item = coupons[index][:item] + " W/COUPON"
+          temp_hash = {
+            :item => coupon_item,
+            :price = > 
+          }
+        end
+      end
+    end
+  end
+  return cart
 end
 
 def apply_clearance(cart)
