c16584174709d05e140d34ae0c3553e5233befe2
Karl Matthes <karlmatthes@gmail.com>
2019-11-06 22:43:44 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -50,10 +50,12 @@ def apply_coupons(cart, coupons)
           }
           cart.push(temp_hash)
           
-          if cart[inner_index][:count] - coupons[index][:num] == 0
-            cart.delete_at(inner_index)
-          else
-            cart[inner_index][:count] -= coupons[index][:num]
+#          if cart[inner_index][:count] - coupons[index][:num] == 0
+#            cart.delete_at(inner_index)
+#          else
+#            cart[inner_index][:count] -= coupons[index][:num]
+
+          cart[inner_index][:count] -= coupons[index][:num]
           end
         end
       end
